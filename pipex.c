b8038929124ff1a22ed67c3877ff781c8abb5e00
serginaAkesandra <sergina.al.al@gmail.com>
2025-08-26 18:54:42 +0200

    handling arguments

--- pipex.c
+++ pipex.c
@@ -6,14 +6,36 @@
 /*   By: asergina <asergina@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 16:15:04 by asergina          #+#    #+#             */
-/*   Updated: 2025/08/11 19:40:23 by asergina         ###   ########.fr       */
+/*   Updated: 2025/08/26 18:53:27 by asergina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include <unistd.h>
 
+void	pipex()
+{
+	int	fd[2];
+	pid_t	pid1;
+	pid_t	pid2;
 
+	if (pipe(fd) == -1)
+		return ;
+	pid1 = fork();
+	if (pid1 == -1)
+		return ;
+	if (pid1 == 0)
+	{
+		//child process
+		
+	}
+	
+}
+
+char	*parsing()
+{
+	
+}
 
 int	main(int argc, char *argv[])
 {

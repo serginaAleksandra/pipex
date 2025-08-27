749c27404c5ba55212ae8a37db6b886d52e6ab87
serginaAleksandra <sergina.al.al@gmail.com>
2025-08-27 22:51:49 +0700

    added parsing

--- pipex.c
+++ pipex.c
@@ -13,9 +13,69 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+char	*parsing(char *cmd, char **envp)
+{
+	int	i
+	char	*path_envp;
+	char	**pathes;
+	char	*path;
+	char	*cmd_path;
 
+	i = 0;
+	while (envp[i])
+	{
+		path_envp = ft_strnstr(envp[i], "PATH=", 5);
+		if(path_envp != NULL)
+			break;
+		i++;
+	}
+	pathes = ft_split(PATH_envp, ":");
+	i = 0;
+	while(pathes[i])
+	{
+		path = ft_strjoin(pathes[i], "/");
+		cmd_path = ft_strjoin(path, cmd);
+		free(path);
+		if (acess(cmd_path, X_OK) == 0)
+			return (cmd_path);
+		free(cmd_path); // ?
+		i++;
+	}
+	return (NULL); //wrong command from argv
+}
 
-int	main(int argc, char *argv[])
+void	execute(char *cmd, char **envp)
+{
+	char	*cmd_path;
+	char	**cmd_argv;
+
+	cmd_argv = ft_split(cmd, ' ');
+	cmd_path = parsing(cmd_argv[0], envp);
+	if (!cmd_path)
+	{
+		perror("command not found");
+		exit(127);
+	}
+	exceve(cmd_path, cmd_argv, envp);
+}
+
+void	pipex()
+{
+	int	fd[2];
+	pid_t	pid;
+
+	if (pipe(fd) == -1)
+		return ;
+	pid = fork();
+	if (pid == -1)
+		return;
+	if (pid == 0)
+		//child process
+	else
+		//parent process
+}
+
+int	main(int argc, char *argv[], char **envp)
 {
 	if (argc == 5)
 	{
